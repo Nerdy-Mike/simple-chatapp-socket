7d342684675b25b7509c206777c27a7b105221bd
Mike-theNerdy <maule.hc@gmail.com>
2021-08-05 01:44:23 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter , Route } from 'react-router-dom';
+
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
+
+
+const App = () => {
+    return (
+    <BrowserRouter >
+        <Route path='/' exact component={Join}/>
+        <Route path='/chat' component={Chat}/>
+        
+    </BrowserRouter >
+);
+}
+
+export default App;
+
+//the path='/chat' may not require exact, because the link maybe something like '/chat/:id'
